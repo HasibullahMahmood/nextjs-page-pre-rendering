e1c419a501cc62b77a307f3cd3d94c9fd9d9fe6b
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2021-09-27 15:36:27 +0300

    getStaticProps implemented

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+import { Component } from 'react';
+import path from 'path';
+import fs from 'fs/promises';
+
+class HomePage extends Component {
+	render() {
+		const { products } = this.props;
+		return (
+			<div>
+				<ul>
+					{products.map((item) => (
+						<li key={item.id}>{item.title}</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
+
+export const getStaticProps = async () => {
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+	return {
+		props: {
+			products: data.products,
+		},
+	};
+};
 
 export default HomePage;
